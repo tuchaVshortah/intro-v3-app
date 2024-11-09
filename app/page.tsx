6a8787d14667934cf3cc5aadc6cbae2a549bbfa7
Nurkanat Baysenkul <nukenbai190104@gmail.com>
2024-11-09 12:54:26 +0500

    use a server side async function

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+const getData = async () => {
+  await new Promise((resolve) => setTimeout(() => resolve(0), 1000));
+  return { data: [1, 2, 3] };
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
+
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <h1 className="text-4xl">Hello World</h1>
