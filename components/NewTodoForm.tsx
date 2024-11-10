5b78b7b50c792038d13d1b8589c5d48c3c4d3134
Nurkanat Baysenkul <nukenbai190104@gmail.com>
2024-11-10 17:36:03 +0500

    add a name to the form

--- components/NewTodoForm.tsx
+++ components/NewTodoForm.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { newTodo } from '@/utils/actions'
 
 const NewTodoForm = ({ }) => {
   return (
     <div>
-      <form action="" className='border border-black/25'>
-        <input type="text" />
+      <form action={newTodo} className='border border-black/25'>
+        <input name="content" type="text" />
+        <button type="submit">new todo</button>
       </form>
     </div>
   )
