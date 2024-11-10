c30a495024758f7610253f5b7a29558201828fba
Nurkanat Baysenkul <nukenbai190104@gmail.com>
2024-11-10 22:39:32 +0500

    update

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -2,8 +2,9 @@ import TodoList from '@/components/TodoList'
 import db from '@/utils/db'
 
 const getData = async () => {
+  // await new Promise((resolve, reject) => setTimeout(() => resolve(0), 2000));
+
   const todos = await db.todo.findMany({})
-  console.log(todos)
   return todos
 }
 
