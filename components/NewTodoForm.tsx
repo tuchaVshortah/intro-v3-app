885c24a1b2de31a2eabf367318fce7ecc597c43f
Nurkanat Baysenkul <nukenbai190104@gmail.com>
2024-11-10 13:35:17 +0500

    add components

---
+++ components/NewTodoForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewTodoForm = () => {
+  return (
+    <div>NewTodoForm</div>
+  )
+}
+
+export default NewTodoForm
---
+++ components/TodoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Todo from './Todo'
+
+interface TodoListProps {
+  todos: { id: number; [key: string]: any }[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  return (
+    <>
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo.id} />
+      ))}
+    </>
+  )
+}
+
+export default TodoList
