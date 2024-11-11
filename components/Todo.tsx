47cff87d9f92479990341b65c6a965eba9ff9dcb
Nurkanat Baysenkul <nukenbai190104@gmail.com>
2024-11-11 11:46:05 +0500

    update components

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,9 +1,13 @@
 'use client'
-import React from 'react'
+import db from '@/utils/db'
+import { revalidatePath } from 'next/cache'
+import { startTransition, useTransition } from 'react'
+import { completeTodo } from '@/utils/actions'
 
 const Todo = ({ todo }) => {
+  const [pending, setPending] = useTransition()
   return (
-    <div>{todo.content}</div>
+    <div className={`border border-black/20 cursor-pointer ${todo.completed ? 'line-through text-gray-900': ''}`} onClick={() => setPending(() => completeTodo(todo.id))}>{todo.content}</div>
   )
 }
 
