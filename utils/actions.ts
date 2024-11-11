f89d6b0c20f6a5fa42f2676fd697610d15cab9cf
Nurkanat Baysenkul <nukenbai190104@gmail.com>
2024-11-11 11:45:32 +0500

    add server action to complete a todo

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,15 @@ interface newTodoProps {
     content: string
 }
 
+export const completeTodo = async (id) => {
+  await db.todo.update({
+    where: { id },
+    data: { completed: true },
+  })
+
+  revalidatePath('/todos')
+}
+
 export const newTodo = async (formData: FormData) => {
     const todo = await db.todo.create({
         data: {
