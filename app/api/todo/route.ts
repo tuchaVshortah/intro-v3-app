4851f7161229899bf589cf51db1f6cda0db56d74
Nurkanat Baysenkul <nukenbai190104@gmail.com>
2024-11-11 11:59:24 +0500

    add a post verb

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -3,3 +3,8 @@ import { NextResponse } from "next/server"
 export const GET = async (request: Request) => {
     return NextResponse.json({ message: "hello" })
 }
+
+export const POST = async (request: Request) => {
+    const data = await request.json()
+    NextResponse.json(data)
+}
